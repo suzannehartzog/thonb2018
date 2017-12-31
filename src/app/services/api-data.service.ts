4e27bddaeefa568001662b79bd6a8cde02b1c7a4
souvik86 <smsouvik.2008@gmail.com>
2017-12-31 22:23:59 +0530

    Create Itinerary & Register Package changes

--- src/app/services/api-data.service.ts
+++ src/app/services/api-data.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Router} from "@angular/router";
 import 'rxjs/add/operator/map';
 
 
 @Injectable()
 export class ApiDataService {
 
+  private apiData = new BehaviorSubject<any>(null);
+  public apiData$ = this.apiData.asObservable();
+
   constructor(
-    private http: Http
+    private http: Http,
+    private router: Router
   ) { }
 
   public ENV_URL_SEARCH = 'https://yayaati.herokuapp.com/';
 
- public getCities() {
-    return this.http.get(this.ENV_URL_SEARCH + '/v1/search/getAllCities').map(
-      (response) =>response.json()
+  public getCities() {
+    return this.http.get(this.ENV_URL_SEARCH + 'v1/search/getAllCities').map(
+      (response) => response.json()
     );
   }
   public createItinerary(product) {
@@ -23,7 +29,7 @@ export class ApiDataService {
       product: product
     };
     let tempVar;
-    return this.http.post(this.ENV_URL_SEARCH + '/v1/booking/createItinerary', params).map(
+    return this.http.post(this.ENV_URL_SEARCH + 'v1/booking/createItinerary', params).map(
       (response) => {
         response.toString();
         if (response.status == 200) {
@@ -37,19 +43,13 @@ export class ApiDataService {
 
   public getResourcesForPackage(product) {
     //POST /v1/booking/getResourcesForPackage
-    console.log(product) ;
-    if(product.pickUpIncluded == "") {
-      product.pickUpIncluded = false;
-    }
-    if(product.dropIncluded == "") {
-      product.dropIncluded = false;
-    }
-    if(product.guideIncluded == "") {
-      product.guideIncluded = false;
-    }
+    console.log(product);    
 
     return this.http.post(this.ENV_URL_SEARCH + '/v1/booking/getResourcesForPackage', product).map(
       (response) => {
+        //return response.json();
+        this.apiData.next(response);
+        this.router.navigate(['register-itinerary']);
       }
     );
   }
@@ -83,15 +83,29 @@ export class ApiDataService {
     return hotelArray;
   }
 
-  public getAllHotelsByOnwer(ownerId){
+  public getAllHotelsByOnwer(ownerId) {
     return this.http.get('assets/service-json/hotel-listing.json').map(
+      (response) => response.json()
+    );
+  }
+  public roomTypeCapacity(hoteld) {
+    return this.http.get('assets/service-json/hotel-room-type.json').map(
+      (response) => response.json()
+    );
+  }
+  public activeAuctions(){
+    return this.http.get('assets/service-json/auction-list.json').map(
       (response) =>response.json()
     );
   }
-  public roomTypeCapacity(hoteld){
-    return this.http.get('assets/service-json/hotel-room-type.json').map(
-      (response) =>response.json()
-    );
+  public checkValidBidder(){
+    return;
+  }
+  public createPromotion(){
+    return;
+  }
+  public createFlashSale(){
+    return;
   }
   // public dummygetNotificationsPosts(){
   //   let param = {
